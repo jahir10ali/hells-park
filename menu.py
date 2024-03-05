467ebd968a335095b4eb3c7d72b4fa8d293d9ad0
Rochak Moktan <moktanrochak@gmail.com>
2024-03-05 16:11:55 +0000

    welcome

--- menu.py
+++ menu.py
@@ -17,13 +17,15 @@ class screen():
         canvas.draw_text(self.start, (450,400), 50, 'Blue', 'serif')
 
 def click_start():
-    frame.set_canvas_background("blue")
+    frame.set_canvas_background("white")
+    frame.set_draw_handler(start_screen.draw)
     
 
 
 
-def click_menu():
+def click_how_to_play():
     frame.set_canvas_background("black")
+    frame.set_draw_handler(tutorial_screen.draw)
     
 
     
@@ -32,6 +34,8 @@ def click_menu():
 
 
 opening_screen = screen(name, "Start")
+start_screen = screen("Choose level", "back")
+tutorial_screen = screen("How to play", "back")
 
 frame = simplegui.create_frame("Welcome Screen", 1000, 700)
 frame.set_canvas_background("white")
@@ -41,7 +45,7 @@ frame.set_canvas_background("white")
 
 
 frame.add_button("Start", click_start)
-frame.add_button("Menu", click_menu)
+frame.add_button("Menu", click_how_to_play)
 
 
 frame.set_draw_handler(opening_screen.draw)

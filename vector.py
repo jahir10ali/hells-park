d67a27caa668f8a7cc2f4775f66855501ba310e8
jahir10ali <137449072+jahir10ali@users.noreply.github.com>
2024-03-05 09:14:36 +0000

    Added the vector.py from Moodle
    This is the Vector class we have to use for our game

---
+++ vector.py
@@ -0,0 +1,70 @@
+import math
+
+# The Vector class
+class Vector:
+
+    # Initialiser
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    # Returns a string representation of the vector
+    def __str__(self):
+        return "(" + str(self.x) + "," + str(self.y) + ")"
+
+    # Tests the equality of this vector and another
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    # Tests the inequality of this vector and another
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    # Returns a tuple with the point corresponding to the vector
+    def get_p(self):
+        return (self.x, self.y)
+
+    # Returns a copy of the vector
+    def copy(self):
+        return Vector(self.x, self.y)
+
+    # Adds another vector to this vector
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __add__(self, other):
+        return self.copy().add(other)
+
+    # Negates the vector (makes it point in the opposite direction)
+    def negate(self):
+        return self.multiply(-1)
+
+    def __neg__(self):
+        return self.copy().negate()
+
+    # Subtracts another vector from this vector
+    def subtract(self, other):
+        return self.add(-other)
+
+    def __sub__(self, other):
+        return self.copy().subtract(other)
+
+    # Multiplies the vector by a scalar
+    def multiply(self, k):
+        self.x *= k
+        self.y *= k
+        return self
+
+    def __mul__(self, k):
+        return self.copy().multiply(k)
+
+    def __rmul__(self, k):
+        return self.copy().multiply(k)
+
+    # Divides the vector by a scalar
+    def divide(self, k):
+        return self.multiply(1/k)
+
+    def __truediv__(self, k):

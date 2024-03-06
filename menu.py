56d630967b9b79c3912acc575fcfe5e5b40dea04
Rochak Moktan <moktanrochak@gmail.com>
2024-03-06 13:22:14 +0000

    menu

--- menu.py
+++ menu.py
@@ -26,7 +26,7 @@ class homescreen():
         self.click_pos = pos
         if self.clicked_inside_button(pos, self.start_button_pos):
             frame.set_draw_handler(start_screen.draw)
-            
+            frame.set_mouseclick_handler(start_screen.click_trigger)
         elif self.clicked_inside_button(pos, self.how_to_play_button_pos):
             frame.set_draw_handler(tutorial_screen.draw)
             frame.set_mouseclick_handler(tutorial_screen.click_trigger)
@@ -42,10 +42,31 @@ class homescreen():
     
 class startscreen():
 
-    def draw(self, canvas):
-        global title_button_pos
-        canvas.draw_text("Choose your level", title_button_pos.get_p(), 50, 'Blue', 'serif')
+    def __init__(self):
+        self.start_game_button_pos = Vector(404,490)
+        self.back_button_pos = Vector(100,100)
 
+    def draw(self, canvas):
+        
+        canvas.draw_text("Level One", (335,200), 100, 'Blue', 'sans-serif')
+        canvas.draw_text("START GAME!", self.start_game_button_pos.get_p(), 40, 'Blue', 'sans-serif')
+        canvas.draw_text("Back", self.back_button_pos.get_p(), 40, 'Blue', 'sans-serif')
+
+    def click_trigger(self, pos):
+
+        self.click_pos = pos
+        if self.clicked_inside(pos, self.start_game_button_pos):
+            frame.set_draw_handler(opening_screen.draw)
+            frame.set_mouseclick_handler(opening_screen.click_trigger)
+        elif self.clicked_inside(pos, self.back_button_pos):
+            frame.set_draw_handler(opening_screen.draw)
+            frame.set_mouseclick_handler(opening_screen.click_trigger)
+        
+    def clicked_inside(self, pos, button_pos):
+        return(
+            self.click_pos[0] > button_pos.get_p()[0] and self.click_pos[0] < button_pos.get_p()[0] + 100 and 
+            self.click_pos[1] > button_pos.get_p()[1] - 50 and self.click_pos[1] < button_pos.get_p()[1]
+        )
 
 class tutorialscreen():
 

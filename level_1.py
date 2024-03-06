bbbe635636c8c4c35899b7942a0638395770ceea
Rochak Moktan <moktanrochak@gmail.com>
2024-03-06 13:21:23 +0000

    lvl 1

--- level_1.py
+++ level_1.py
@@ -1,5 +1,10 @@
-print("get ready for level 1!")
+try:
+    import Simplegui
+except ImportError:
+    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-
+frame = simplegui.create_frame("Level one", 1000,700)
+frame.set_canvas_background("Black")
+frame.start()
 
 # this is the level 1 screen of the game to be worked on

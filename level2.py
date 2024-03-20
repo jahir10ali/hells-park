5c98064ce9f4180ccfcdb9582b680771f3eafd85
jahir10ali <jahir10ali@gmail.com>
2024-03-20 11:28:09 +0000

    making level 2 based on designs

--- level2.py
+++ level2.py
@@ -18,32 +18,16 @@ move_speed = 5
 is_moving_left = False
 is_moving_right = False
 
-# Platform variables
-platform_height = 20
-platform_gap = 150
-platform_width = 200
-platform_positions = [(100, 500), (350, 400), (600, 300), (250, 200)]
-
 exit_btn_img = 'https://i.ibb.co/r29NXsx/exit-btn.jpg'
 
 def draw(canvas):
     global exit_btn, block_pos
-    canvas.draw_text("THIS IS LEVEL 2", (130, 150), 50, "White", 'monospace')
-    exit_btn = draw_button(canvas, exit_btn_img, 750, 20, 125, 50)
-    # Draw block
+    exit_btn = draw_button(canvas, exit_btn_img, 750, 20, 93.75, 37.5)
     canvas.draw_polygon([(block_pos.x - block_size / 2, block_pos.y - block_size / 2),
                          (block_pos.x + block_size / 2, block_pos.y - block_size / 2),
                          (block_pos.x + block_size / 2, block_pos.y + block_size / 2),
                          (block_pos.x - block_size / 2, block_pos.y + block_size / 2)],
                         1, 'Red', 'Red')
-    
-    # Draw platforms
-    for pos in platform_positions:
-        canvas.draw_polygon([(pos[0] - platform_width / 2, pos[1] - platform_height / 2),
-                             (pos[0] + platform_width / 2, pos[1] - platform_height / 2),
-                             (pos[0] + platform_width / 2, pos[1] + platform_height / 2),
-                             (pos[0] - platform_width / 2, pos[1] + platform_height / 2)],
-                            1, 'Green', 'Green')
 
 def keydown(key):
     global is_jumping, is_moving_left, is_moving_right
@@ -52,7 +36,7 @@ def keydown(key):
     elif key == simplegui.KEY_MAP['right']:
         is_moving_right = True
     elif key == simplegui.KEY_MAP['up'] and not is_jumping:
-        is_jumping = True
+        jump()
 
 def keyup(key):
     global is_moving_left, is_moving_right
@@ -61,8 +45,13 @@ def keyup(key):
     elif key == simplegui.KEY_MAP['right']:
         is_moving_right = False
 
+def jump():
+    global is_jumping, jump_strength
+    is_jumping = True
+    jump_strength = 15  # Reset jump strength when jumping
+
 def update():
-    global block_pos, is_jumping, jump_strength, gravity
+    global block_pos, is_jumping, is_moving_left, is_moving_right, jump_strength
 
     # Move left if allowed
     if is_moving_left and block_pos.x - block_size / 2 > 0:
@@ -79,16 +68,6 @@ def update():
 
         if block_pos.y >= canvas_height - block_size / 2:
             is_jumping = False
-            jump_strength = 15  # Reset jump strength when landing
-
-        # Check for collision with platforms
-        for pos in platform_positions:
-            if (pos[0] - platform_width / 2 <= block_pos.x <= pos[0] + platform_width / 2 and
-                pos[1] - platform_height / 2 <= block_pos.y + block_size / 2 <= pos[1] + platform_height / 2):
-                is_jumping = False
-                jump_strength = 15
-                block_pos.y = pos[1] - platform_height / 2 - block_size / 2
-                break
 
 def click(pos, frame):
     global exit_btn
@@ -97,7 +76,6 @@ def click(pos, frame):
         frame.set_draw_handler(levels.draw)
         frame.set_mouseclick_handler(lambda pos: levels.click(pos, frame))
         timer.stop()
-
 
 timer = simplegui.create_timer(1000 // 60, update)
 timer.start()

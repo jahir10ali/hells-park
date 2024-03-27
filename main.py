2bab5c1ec01a80a72bddeb48645893aeb9f3dceb
jahir10ali <jahir10ali@gmail.com>
2024-03-27 12:49:58 +0000

    comments + documentation

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
+''' 
+This is the main.py file. This was created to only instantiate a frame. 
+It allows other files to set the frame draw handler to its own draw(canvas) method.
+This file sets the draw handlaer to the draw() method in the menu.py file as this is the first screen.
+'''
+
 try:
     import simplegui
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
-# Define canvas dimensions
+# These are the canvas dimensions
 canvas_width = 900
 canvas_height = 600
 
-# Create a global frame
+# This is to create a global frame
 frame = simplegui.create_frame('Hells Park', canvas_width, canvas_height, 0)
 
-# Import other modules
+# This is to make use of the menu draw() function
 import menu
 
-# Set the initial draw and mouse click handlers
+# This set the initial draw and mouse click handlers to the ones in the menu.py file
 frame.set_draw_handler(menu.draw)
-frame.set_mouseclick_handler(lambda pos: menu.click(pos, frame))  # Pass frame to click function
+frame.set_mouseclick_handler(lambda pos: menu.click(pos, frame))  
 
 # Start the frame
 frame.start()
--- menu.py
+++ menu.py
@@ -1,4 +1,11 @@
-from imagesANDbuttons import draw_button, draw_image
+'''
+This is the menu.py file. This file was created to draw the menu elements
+including the title and the buttons. It redirects the player to the level.py or
+howtoplay.py screens if its respective buttons are clicked.
+'''
+
+from imagesANDbuttons import draw_button, draw_image 
+# This import is for drawing buttons and image
 
 try:
     import simplegui
@@ -10,9 +17,10 @@ play_btn_img = 'https://i.ibb.co/KFG5ms3/play-btn.jpg'
 htp_btn_img = 'https://i.ibb.co/2kCBLmH/htp-btn.jpg'
 exit_btn_img = 'https://i.ibb.co/r29NXsx/exit-btn.jpg'
 title_img = 'https://i.ibb.co/PjQb0W9/title.png'
+# These are the button image assets
 
 
-# Define draw function to load and draw the image
+# This is the draw function of the menu, drawing the title and buttons on screen
 def draw(canvas):      
     global play_btn, htp_btn, exit_btn
     
@@ -20,20 +28,20 @@ def draw(canvas):
     htp_btn = draw_button(canvas, htp_btn_img, 150, 450, 250, 100)
     exit_btn = draw_button(canvas, exit_btn_img, 750, 20, 125, 50)
     title = draw_button(canvas, title_img, 195, 85, 500, 100)
-    
-    #canvas.draw_text("Welcome to Hell's Park", (140, 150), 50, "White", 'monospace')
 
 
-def click(pos, frame):  # Add frame as a parameter
+# This is the click() function that handles mouse clicks on these button images
+def click(pos, frame):
     global play_btn, htp_btn
     if play_btn.is_clicked(pos):
         import levels
-        # Change the draw and mouse click handlers directly from game_test.py
-        frame.set_draw_handler(levels.draw)
-        frame.set_mouseclick_handler(lambda pos: levels.click(pos, frame))  # Pass frame to click function
+        frame.set_draw_handler(levels.draw) # This switches the frame to the levels.py content
+        frame.set_mouseclick_handler(lambda pos: levels.click(pos, frame))
+        # This switches the handle of mouse clicks to the levels.py click() function
     elif htp_btn.is_clicked(pos):
         import howtoplay
-        frame.set_draw_handler(howtoplay.draw)
+        frame.set_draw_handler(howtoplay.draw) # This switches the frame to the howtoplay.py content
         frame.set_mouseclick_handler(lambda pos: howtoplay.click(pos, frame))
+        # This switches the handle of mouse clicks to the howtoplay.py click() function
     elif exit_btn.is_clicked(pos):
-        frame.stop()
+        frame.stop() # This stops the frame, acting as a normal exit button
